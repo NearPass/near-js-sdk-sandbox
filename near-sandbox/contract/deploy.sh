1d326097513efdd7b97f0e2a4d0983644420bc32
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-21 14:20:06 +0530

    fix: setMetadata on NFT contract

--- near-sandbox/contract/deploy.sh
+++ near-sandbox/contract/deploy.sh
@@ -12,3 +12,6 @@ echo ">> Deploying contract"
 # https://docs.near.org/tools/near-cli#near-dev-deploy
 echo '{ "nft_contract_id": "'"$CONTRACT"'" }'
 near dev-deploy --wasmFile build/events.wasm --initFunction init --initArgs '{ "nft_contract_id": "'"$CONTRACT"'" }'
+export EVENTS=$(cat ./neardev/dev-account)
+near call "$CONTRACT" setOwnerId '{ "owner_id": "'"$EVENTS"'" }' --accountId "$EVENTS"
+near call "$CONTRACT" setMetadata '{ "metadata": { "spec": "nft-1.0.0", "name": "NearPass", "symbol": "NP" } }' --accountId "$EVENTS"
